a8f18db596d3774e20b3287dfbb274ba78590455
Andy Baker <andy@andybak.net>
2019-10-24 11:39:02 +0100

    Initial commit

---
+++ Assets/TREE/Scripts/MakeTree.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Serialization;
+
+namespace TREESharp {
+	
+public class MakeTree : MonoBehaviour {
+	
+	TREE tree;
+	public GameObject defaultJoint;
+	public TreeTransform xForm { get; set; }
+
+	[Tooltip("The number of joints on a particular branch layer ")]
+	public string joints = "10";
+	
+	[Tooltip("How often the branch sprouts new branches (every 1st, 2nd, 5th joint etc) ")]
+	public string divs = "1";
+
+	[Tooltip("At which joint the branch starts branching")]
+	public string start = "0";
+	
+	[Tooltip("At which joint the branch stops branching")]
+	public string end = "-1";
+	
+	[Tooltip("The angle that new branches will be rotated to ")]
+	public string angles = "0";
+	
+	[Tooltip("How long each joint on a branch is ")]
+	public string length = "1";
+	
+	[Tooltip("How many branches sprout from each joint ")]
+	public string rads = "1";
+	
+	
+
+	public string[] selectJoints;
+	public List<Transformer> Transformers;
+	
+	[FormerlySerializedAs ("LegacytransformJoints")]
+	public string[] LegacyTransformJoints;
+
+	public bool animate = true;
+
+	public float counter { get; set; }
+	public float countSpeed { get; set; }
+
+	public float timeScale = 1;
+
+	public bool reSetupXform = true;
+	public bool rebuildTree = true;
+	
+	public void buildTree(){
+		
+		if (GetComponent<Joint> () != null) {
+			if (GetComponent<Joint>().limbs.Count > 0) {
+				GetComponent<Joint>().limbs.Clear();
+			}
+		}
+
+		if (gameObject.GetComponent<TREE>() == null) {
+			tree = gameObject.AddComponent<TREE>();
+		}
+		else
+		{
+			for (int i=0; i<transform.childCount; i++) {
+				Destroy (transform.GetChild (0).gameObject);
+			}
+			tree = GetComponent<TREE>();
+		}
+			
+		tree.setDefaultJoint(defaultJoint);
+
+		tree.generate (
+			"joints",	joints,
+			"rads",		rads,
+			"angles",	angles,
+			"length",	length,
+			"divs",		divs,
+			"start",	start,
+			"end",		end
+		);
+
+		tree.jointDictionary.Clear();
+		TREEUtils.makeDictionary(tree.gameObject);
+
+	}
+	
+	void Update ()
+	{
+		
+		if (rebuildTree) {
+			rebuildTree = false;
+			buildTree();
+			reSetupXform = true;
+		}
+		else if (reSetupXform)
+		{
+			reSetupXform = false;
+			if (gameObject.GetComponent<TreeTransform> () == null) {
+				xForm = gameObject.AddComponent<TreeTransform> ();
+			} else
+				xForm = gameObject.GetComponent<TreeTransform> ();
+			
+			xForm.Setup(selectJoints, Transformers, tree);
+			Animate();
+		}
+		else if (animate && !reSetupXform && !rebuildTree)
+		{
+			xForm.Animate(Time.time * timeScale);			
+		}
+	}
+
+	public void Animate(){
+		counter += countSpeed * timeScale;
+		xForm.Animate(counter);
+	}
+
+	public void Animate(float offset){
+		counter += offset;
+		xForm.Animate (counter);
+	}
+		
+}
+	
+}
---
+++ Assets/VFXHierarchy2AttributeMapBinder.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections.Generic;
+using TREESharp;
+using UnityEngine.VFX;
+
+namespace UnityEngine.VFX.Utility
+{
+    [AddComponentMenu("VFX/Property Binders/Hierarchy 2 to Attribute Map Binder")]
+    [VFXBinder("Point Cache/Hierarchy 2 to Attribute Map")]
+    class VFXHierarchy2AttributeMapBinder : VFXBinderBase
+    {
+        [VFXPropertyBinding("System.UInt32"), SerializeField]
+        protected ExposedProperty m_BoneCount = "BoneCount";
+
+        [VFXPropertyBinding("UnityEngine.Texture2D"), SerializeField]
+        protected ExposedProperty m_PositionMap = "PositionMap";
+
+        [VFXPropertyBinding("UnityEngine.Texture2D"), SerializeField]
+        protected ExposedProperty m_TargetPositionMap = "TargetPositionMap";
+
+        [VFXPropertyBinding("UnityEngine.Texture2D"), SerializeField]
+        protected ExposedProperty m_ScaleMap = "ScaleMap";
+        
+        [VFXPropertyBinding("UnityEngine.Texture2D"), SerializeField]
+        protected ExposedProperty m_ColorsMap = "ColorsMap";
+
+        public Transform HierarchyRoot = null;
+        public uint MaximumDepth = 3;
+        public string nameFilter;
+        public bool doUpdateHierarchy;
+
+        private Texture2D position;
+        private Texture2D targetPosition;
+        private Texture2D scale;
+        private Texture2D colors;
+        private List<Bone> bones;
+
+        private struct Bone
+        {
+            public Transform source;
+            //public float sourceScale;
+            public Transform target;
+            public MeshRenderer mr;
+//            public float targetScale;
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            UpdateHierarchy();
+            if (HierarchyRoot == null || HierarchyRoot.gameObject.activeSelf == false)
+            {
+                HierarchyRoot = FindObjectOfType<TREE>().transform;
+            }
+        }
+
+        void OnValidate()
+        {
+            UpdateHierarchy();
+        }
+
+
+        void UpdateHierarchy()
+        {
+            bones = ChildrenOf(HierarchyRoot, MaximumDepth);
+            int count = bones.Count;
+            Debug.Log("Found Bone Count: " + count);
+
+            position = new Texture2D(count, 1, TextureFormat.RGBAHalf, false, true);
+            targetPosition = new Texture2D(count, 1, TextureFormat.RGBAHalf, false, true);
+            scale = new Texture2D(count, 1, TextureFormat.RGBAHalf, false, true);
+            colors = new Texture2D(count, 1, TextureFormat.RGBAHalf, false, true);
+
+            UpdateData();
+        }
+
+        List<Bone> ChildrenOf(Transform source, uint depth)
+        {
+            List<Bone> output = new List<Bone>();
+            if (source == null) return output;
+            foreach (Transform child in source)
+            {
+                if (nameFilter == "" || child.gameObject.name == nameFilter)
+                {
+                    MeshRenderer mr;
+                    mr = child.GetComponent<MeshRenderer>();
+                    if (mr==null)
+                    {
+                        mr = child.GetComponentInChildren<MeshRenderer>();
+                    }
+
+                    output.Add(new Bone()
+                    {
+                        source = source.transform,
+                        target = child.transform,
+                        mr = mr
+                        //sourceScale = DefaultScale,
+                        //targetScale = DefaultScale,
+                    });
+                }
+
+                if (depth > 0)
+                {
+                    output.AddRange(ChildrenOf(child, depth-1));
+                }
+            }
+            return output;
+        }
+
+
+        void UpdateData()
+        {
+            int count = bones.Count;
+            if (position.width != count) return;
+
+            List<Color> positionList = new List<Color>();
+            List<Color> targetList = new List<Color>();
+            List<Color> scaleList = new List<Color>();
+            List<Color> colorsList = new List<Color>();
+
+            for (int i = 0; i < count; i++)
+            {
+                Bone b = bones[i];
+                positionList.Add(new Color(b.source.position.x, b.source.position.y, b.source.position.z, 1));
+                targetList.Add(new Color(b.target.position.x, b.target.position.y, b.target.position.z, 1));
+                scaleList.Add(new Color(b.source.lossyScale.x, b.source.lossyScale.y, b.source.lossyScale.z, 1));
+                colorsList.Add(b.mr.material.color);
+            }
+            position.SetPixels(positionList.ToArray());
+            targetPosition.SetPixels(targetList.ToArray());
+            scale.SetPixels(scaleList.ToArray());
+            colors.SetPixels(colorsList.ToArray());
+
+            position.Apply();
+            targetPosition.Apply();
+            scale.Apply();
+        }
+
+
+        public override bool IsValid(VisualEffect component)
+        {
+            return HierarchyRoot != null
+                && component.HasTexture(m_PositionMap)
+                && component.HasTexture(m_TargetPositionMap)
+                && component.HasTexture(m_ScaleMap)
+                && component.HasTexture(m_ColorsMap)
+                && component.HasUInt(m_BoneCount);
+        }
+
+        public override void UpdateBinding(VisualEffect component)
+        {
+
+            if (bones.Count == 0 || doUpdateHierarchy)
+            {
+                UpdateHierarchy();
+                doUpdateHierarchy = false;
+            }
+            else
+            {
+                UpdateData();
+            }
+
+            component.SetUInt(m_BoneCount, (uint)bones.Count);
+            component.SetTexture(m_PositionMap, position);
+            component.SetTexture(m_TargetPositionMap, targetPosition);
+            component.SetTexture(m_ScaleMap, scale);
+            component.SetTexture(m_ColorsMap, colors);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Hierarchy: {0} -> {1}", HierarchyRoot == null ? "(null)" : HierarchyRoot.name, m_PositionMap);
+        }
+    }
+}
